ef8bd434e4f627505f5d7461d69c9a6459fd9424
leotor4 <70244766+leotor4@users.noreply.github.com>
2022-04-07 14:08:01 -0300

    traduz código p/ português

--- index.js
+++ index.js
@@ -1,76 +1,72 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
-  }
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
+class Grafo {
+    constructor(){
+        this.listaAdjacencia = {}
     }
-  }
-  addEdge(v1, v2) {
-    this.adjacencyList[v1].push(v2);
-  }
+
+    addVertice(vertice){
+        if(!this.listaAdjacencia[vertice]){
+            this.listaAdjacencia[vertice] = [];
+        }
+    }
+
+    addAresta(v1, v2){
+        this.listaAdjacencia[v1].push(v2);
+    }
 }
 
-const graph = (function () {
-  const g = new Graph();
-  /* ["A", "B", "C", "D", "E"].forEach((v) => g.addVertex(v));
-  g.addEdge("A", "B");
-  g.addEdge("A", "C");
-  g.addEdge("B", "D");
-  g.addEdge("C", "D");
-  g.addEdge("D", "E"); */
-  [
-    "SHIRT",
-    "TIE",
-    "JACKET",
-    "BELT",
-    "UNDERSHORTS",
-    "PANTS",
-    "SHOES",
-    "SOCKS",
-    "WATCH",
-  ].forEach((v) => g.addVertex(v));
-  g.addEdge("SHIRT", "TIE");
-  g.addEdge("TIE", "JACKET");
-  g.addEdge("SHIRT", "BELT");
-  g.addEdge("BELT", "JACKET");
-  g.addEdge("UNDERSHORTS", "PANTS");
-  g.addEdge("PANTS", "BELT");
-  g.addEdge("PANTS", "SHOES");
-  g.addEdge("UNDERSHORTS", "SHOES");
-  g.addEdge("SOCKS", "SHOES");
-  return g;
-})();
+const grafo = (
+    () => {
+        const g = new Grafo();
 
-function dfsTopSortHelper(v, n, visited, topNums) {
-  visited[v] = true;
-  const neighbors = graph.adjacencyList[v];
-  for (const neighbor of neighbors) {
-    if (!visited[neighbor]) {
-      n = dfsTopSortHelper(neighbor, n, visited, topNums);
+        [
+            "CAMISA",
+            "COLAR",
+            "BLAZER",
+            "CINTO",
+            "CUECA/CALCINHA",
+            "CALCA",
+            "SAPATOS",
+            "MEIAS",
+            "WATCH",
+          ].forEach((v) => g.addVertice(v));
+          g.addAresta("CAMISA", "COLAR");
+          g.addAresta("COLAR", "BLAZER");
+          g.addAresta("CAMISA", "CINTO");
+          g.addAresta("CINTO", "BLAZER");
+          g.addAresta("CUECA/CALCINHA", "CALCA");
+          g.addAresta("CALCA", "CINTO");
+          g.addAresta("CALCA", "SAPATOS");
+          g.addAresta("CUECA/CALCINHA", "SAPATOS");
+          g.addAresta("MEIAS", "SAPATOS");
+        return g;
     }
-  }
-  topNums[v] = n;
-  return n - 1;
+)();
+
+const dfs = (v, n, visitado, topNums) => {
+    visitado[v] = true;
+    const vizinhos = grafo.listaAdjacencia[v];
+
+    for(const vizinho of vizinhos) {
+        if(!visitado[vizinho]){
+            n = dfs(vizinho, n, visitado, topNums);
+        }
+    }
+    topNums[v] = n;
+    return n - 1;
 }
 
-function dfsTopSort(graph) {
-  const vertices = Object.keys(graph.adjacencyList);
-  const visited = {};
-  const topNums = {};
-  let n = vertices.length - 1;
-  for (const v of vertices) {
-    if (!visited[v]) {
-      n = dfsTopSortHelper(v, n, visited, topNums);
+const topSort = (grafo) => {
+    const vertices = Object.keys(grafo.listaAdjacencia);
+    const visitado = {};
+    const topNums = {};
+    let n = vertices.length - 1; 
+
+    for(const v of vertices) {
+        if(!visitado[v]) {
+            n = dfs(v, n, visitado, topNums, grafo);
+        }
     }
-  }
-  return topNums;
+    return topNums;
 }
 
-console.log(dfsTopSort(graph));
-// { E: 4,
-//   D: 3,
-//   B: 2,
-//   C: 1,
-//   A: 0 }
+console.log(topSort(grafo));
